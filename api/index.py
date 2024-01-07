fad522ccbae388bc9c0a40caa170812b3f8de73b
OshDubh <oshdubh@gmail.com>
2024-01-07 23:39:15 +0200

    set up the interface between a form POST and the python example scheduling script

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from .google_nurse_scheduling import solve_nurse_scheduling
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+# nurse scheduling
+@app.post("/api/test-algorithm/")
+async def generate_example_schedule(num_employees: int = Form(), num_shifts: int = Form(), num_days: int = Form(), solution_limit: int = Form()):
+    return {"result": solve_nurse_scheduling(num_employees, num_shifts, num_days, solution_limit)}
