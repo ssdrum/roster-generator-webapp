e758e5c0dd87b7d93201ffbbf800c5e63de900d5
ssdrum <luigi.dipaolo02@gmail.com>
2024-01-08 12:22:05 +0100

    added key prop to users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   return (
     <>
       {users.map((user) => {
-        return <h1>{user.name}</h1>;
+        return <h1 key={user.id}>{user.name}</h1>;
       })}
     </>
   );
