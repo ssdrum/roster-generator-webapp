800d7ae20b046dd8a6b757fd995ba139bbd5a7be
OshDubh <60195526+OshDubh@users.noreply.github.com>
2024-01-10 21:32:35 +0200

    Simple homepage (#3)
    * added shadcn card
    
    * changed page to have login details + buttons for the two pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
 import { getUserSession } from '@/app/lib/session';
+import Link from 'next/link';
+
+import { buttonVariants } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default async function Home() {
   const user = await getUserSession();
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-24 space-y-8">
+      <Card>
+        <CardHeader>
+          <CardTitle className='text-lg'>Login Details</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p> Name: { user.name } </p>
+          <p> Email: { user.email } </p>
+          <p> Image URL: { user.image } </p>
+          <p> ID: { user.id } </p>
+        </CardContent>
+      </Card>
 
-  return <main className=''>{JSON.stringify(user)}</main>;
+      <span className="space-x-8">
+        <Link href="/dashboard" className={buttonVariants()}>Dashboard</Link>
+        <Link href="/users" className={buttonVariants()}>Users</Link>
+      </span>
+
+    </div>
+  )
 }
