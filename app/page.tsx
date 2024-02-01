2c2fbabcdbbe0f3fcacd010ef4341da09b5f0649
OshDubh <60195526+OshDubh@users.noreply.github.com>
2024-02-01 09:12:20 +0000

    Landing page and redirecting to dashboard if logged in (#4)
    * deleted nextjs template assets
    
    * temporary assets for the home page
    
    * removed auth protection from '/'
    
    * created a proper landing page and implemented redirect to dashboard if user is logged in
    
    * now using next/image and escaped required characters
    
    * prettier auto-format

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,62 @@
 import { getUserSession } from '@/app/lib/session';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import Image from 'next/image';
 
-import { buttonVariants } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 export default async function Home() {
   const user = await getUserSession();
-  return (
-    <div className='flex min-h-screen flex-col items-center justify-center space-y-8 p-24'>
-      <Card>
-        <CardHeader>
-          <CardTitle className='text-lg'>Login Details</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p> Name: {user.name} </p>
-          <p> Email: {user.email} </p>
-          <p> Image URL: {user.image} </p>
-          <p> ID: {user.id} </p>
-        </CardContent>
-      </Card>
+  // redirect if logged in
+  if (user) {
+    redirect('/dashboard');
+  }
 
-      <span className='space-x-8'>
-        <Link href='/dashboard' className={buttonVariants()}>
-          Dashboard
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center bg-white'>
+      <div className='fixed left-0 top-0 h-full w-full overflow-hidden'>
+        <Image
+          alt=''
+          className='absolute left-0 top-0 h-full w-full object-cover blur-lg'
+          height='1000'
+          src='/blob.svg'
+          style={{
+            aspectRatio: '1000/1000',
+            objectFit: 'cover',
+            filter: 'blur(200px)',
+          }}
+          width='1000'
+        />
+      </div>
+      <div className='z-10 px-48 py-16 text-center'>
+        <h1 className='text-5xl font-bold text-gray-900'>
+          Your employee{"'"}s schedule, at the{' '}
+          <span className='bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-[length:100%_6px] bg-bottom bg-no-repeat'>
+            click of a button.
+          </span>
+        </h1>
+        <p className='mt-8 text-lg text-gray-600'>
+          RosterGenApp generates your work schedule for you, so you don{"'"}t
+          ever have to go through the exhausting process of creating it ever
+          again. Fitted to your exact needs, powered by advanced algorithms.
+        </p>
+        <Link href='/dashboard'>
+          <Button className='mt-8'>Get started</Button>
         </Link>
-        <Link href='/users' className={buttonVariants()}>
-          Users
-        </Link>
-      </span>
+      </div>
+      <div className='z-10 mt-10 w-full md:w-1/2'>
+        <Image
+          alt='Example'
+          className='w-full rounded-t-lg'
+          height='600'
+          src='/example.png'
+          style={{
+            aspectRatio: '1200/600',
+            objectFit: 'cover',
+          }}
+          width='1200'
+        />
+      </div>
     </div>
   );
 }
