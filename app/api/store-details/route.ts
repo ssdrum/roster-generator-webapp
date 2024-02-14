94747809c9d23002f1239f6deb8dfe263e6c6b5e
OshDubh <oshdubh@gmail.com>
2024-02-14 15:01:18 +0000

    fixed tsc errors where tempID could be null

--- app/api/store-details/route.ts
+++ app/api/store-details/route.ts
@@ -50,7 +50,7 @@ export async function POST(req: any) {
     numEmployeesAssigned.map(async (numEmployeesAssignedData: any) => {
       const { shiftId, assignments } = numEmployeesAssignedData;
 
-      const shift = await prisma.shift.findUnique({
+      const shift = await prisma.shift.findFirst({
         where: {
           tempID: shiftId,
         },
