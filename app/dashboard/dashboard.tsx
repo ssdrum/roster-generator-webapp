70ca05388d16ee005544489c534f46260b7a9b7a
ssdrum <luigi.dipaolo02@gmail.com>
2024-02-17 17:08:47 +0000

    Add generate button in dashboard

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -1,6 +1,10 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState } from 'react';
 import Roster from './components/roster';
 import { Employee, NumEmployeesAssigned, Shift } from '@prisma/client';
+import ButtonLoading from './components/button-loading';
+import { Button } from '../ui/shadcn/button';
 
 type Props = {
   employees: Employee[];
@@ -9,10 +13,13 @@ type Props = {
 };
 
 const Dashboard: FC<Props> = ({ employees, shifts, numEmployeesAssigned }) => {
-  // This is a server-component, ergo the logs will also happen server side (not in the browser)
-  console.log(employees);
-  console.log(shifts);
-  console.log(numEmployeesAssigned);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const handleClick = async () => {
+    setIsGenerating(true);
+    console.log('clicked');
+    setIsGenerating(false);
+  };
 
   // temporary assignments data
   const assignments = [
@@ -36,29 +43,18 @@ const Dashboard: FC<Props> = ({ employees, shifts, numEmployeesAssigned }) => {
         { name: 'Evening', startTime: '4:00 PM', endTime: '12:00 AM' },
       ],
     },
-    {
-      employee: 'Bob',
-      shifts: [
-        { name: 'Morning', startTime: '8:00 AM', endTime: '4:00 PM' },
-        null,
-        null,
-        { name: 'Afternoon', startTime: '12:00 PM', endTime: '8:00 PM' },
-        null,
-      ],
-    },
-    {
-      employee: 'Alice',
-      shifts: [
-        null,
-        null,
-        { name: 'Morning', startTime: '8:00 AM', endTime: '4:00 PM' },
-        { name: 'Afternoon', startTime: '12:00 PM', endTime: '8:00 PM' },
-        { name: 'Evening', startTime: '4:00 PM', endTime: '12:00 AM' },
-      ],
-    },
   ];
 
-  return <Roster assignments={assignments} />;
+  return (
+    <>
+      <Roster assignments={assignments} />
+      {isGenerating ? (
+        <ButtonLoading />
+      ) : (
+        <Button onClick={handleClick}>Generate Roster</Button>
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
