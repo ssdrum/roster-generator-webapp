5e3d9ebbd4b9103af33eecf293015579b8dea6b5
ssdrum <luigi.dipaolo02@gmail.com>
2024-02-17 14:05:11 +0000

    Remove old migrations

--- prisma/migrations/20240214131656_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `tempID` on the `shifts` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "shifts" DROP COLUMN "tempID";
--- prisma/migrations/20240217140420_/migration.sql
+++ prisma/migrations/20240217140420_/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "users" (
     "email" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
+    "workDays" INTEGER[],
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -41,6 +42,24 @@ CREATE TABLE "assignments" (
     CONSTRAINT "assignments_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "num_employees_assigned" (
+    "id" TEXT NOT NULL,
+    "shift" TEXT NOT NULL,
+
+    CONSTRAINT "num_employees_assigned_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "count_per_day" (
+    "id" TEXT NOT NULL,
+    "day" INTEGER NOT NULL,
+    "numAssigned" INTEGER NOT NULL,
+    "numEmployeesAssignedId" TEXT,
+
+    CONSTRAINT "count_per_day_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
@@ -61,3 +80,9 @@ ALTER TABLE "assignments" ADD CONSTRAINT "assignments_shift_fkey" FOREIGN KEY ("
 
 -- AddForeignKey
 ALTER TABLE "assignments" ADD CONSTRAINT "assignments_assignedBy_fkey" FOREIGN KEY ("assignedBy") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "num_employees_assigned" ADD CONSTRAINT "num_employees_assigned_shift_fkey" FOREIGN KEY ("shift") REFERENCES "shifts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "count_per_day" ADD CONSTRAINT "count_per_day_numEmployeesAssignedId_fkey" FOREIGN KEY ("numEmployeesAssignedId") REFERENCES "num_employees_assigned"("id") ON DELETE SET NULL ON UPDATE CASCADE;
