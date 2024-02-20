cd67e6888b886938a844f5eb09e943dd69bbf4a8
ssdrum <luigi.dipaolo02@gmail.com>
2024-02-20 15:26:36 +0000

    Remove migrations

--- prisma/migrations/20240219120149_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `email` on table `employees` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "employees" ALTER COLUMN "email" SET NOT NULL;
--- prisma/migrations/20240220132518_remove_unique_constraint_on_employee_email/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "employees_email_key";
