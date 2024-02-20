441cfe2ca91fa57ce5af801c2b3c320cc4f1c69e
ssdrum <luigi.dipaolo02@gmail.com>
2024-02-20 11:33:48 +0000

    Add check that redirects user to / if session is undefined

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,6 +8,7 @@ import {
 } from '../lib/data';
 import { getUserSession } from '@/app/lib/session';
 import DashboardProvider from '@/app/dashboard/dashboard-context';
+import { redirect } from 'next/navigation';
 
 export default async function DashboardLayout({
   children, // will be a page or nested layout
@@ -15,27 +16,31 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   const session: Session = await getUserSession();
-  const userId = session.id;
-  // Fetch all data related to user server-side
-  const userData = await fetchUserData(userId);
-  const employees = await fetchEmployees(userId);
-  const shifts = await fetchShifts(userId);
-  const numEmployeesAssigned = await fetchNumEmployeesAssigned(userId);
+  if (session === undefined) {
+    redirect('/');
+  } else {
+    // Fetch all data related to user server-side
+    const userId = session.id;
+    const userData = await fetchUserData(userId);
+    const employees = await fetchEmployees(userId);
+    const shifts = await fetchShifts(userId);
+    const numEmployeesAssigned = await fetchNumEmployeesAssigned(userId);
 
-  return (
-    <div className='flex'>
-      <Navbar session={session} />
-      <div className='container mx-auto my-5'>
-        {/* Context provider provides access to variables below to all components in /dashboard */}
-        <DashboardProvider
-          user={userData}
-          employees={employees}
-          shifts={shifts}
-          numEmployeesAssigned={numEmployeesAssigned}
-        >
-          {children}
-        </DashboardProvider>
+    return (
+      <div className='flex'>
+        <Navbar session={session} />
+        <div className='container mx-auto my-5'>
+          {/* Context provider provides access to variables below to all components in /dashboard */}
+          <DashboardProvider
+            user={userData}
+            employees={employees}
+            shifts={shifts}
+            numEmployeesAssigned={numEmployeesAssigned}
+          >
+            {children}
+          </DashboardProvider>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
