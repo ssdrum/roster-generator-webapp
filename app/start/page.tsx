b1633340f772b22de9aae93a2cf3c16cea4339be
ssdrum <luigi.dipaolo02@gmail.com>
2024-02-22 17:48:18 +0000

    Remove pre-populated items in start form

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -32,11 +32,10 @@ const StartForm = () => {
 
   // Initialise form with default values on first render
   const firstShiftId = uuidv4();
-  const secondShiftId = uuidv4();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema), // Link the react form and the resolver together for validation
     defaultValues: {
-      workDays: [0, 1, 2, 3, 4],
+      workDays: [0, 1, 2, 3, 4], // Pre-select Monday-Friday
       shifts: [
         {
           id: firstShiftId,
@@ -44,37 +43,13 @@ const StartForm = () => {
           startTime: '08:00',
           endTime: '12:00',
         },
-        {
-          id: secondShiftId,
-          name: 'Evening',
-          startTime: '15:00',
-          endTime: '21:00',
-        },
       ],
       numDaysOff: 0,
       employees: [
         {
-          id: '0',
-          name: 'Good Employee',
-          email: 'good@mail.com',
-          createdBy: '',
-        },
-        {
-          id: '1',
-          name: 'Bad Employee',
-          email: 'bad@mail.com',
-          createdBy: '',
-        },
-        {
-          id: '2',
-          name: 'Luigi',
-          email: 'luigi@mail.com',
-          createdBy: '',
-        },
-        {
-          id: '3',
-          name: 'Oisin',
-          email: 'oisin@mail.com',
+          id: '-1',
+          name: '',
+          email: '',
           createdBy: '',
         },
       ],
